84af44adf4b86b534724bf3d48b69906b502af30
sramos <sframos83@gmail.com>
2015-08-15 22:09:40 +0100

    Changes

--- plugin.video.redcouch/addons.py
+++ plugin.video.redcouch/addons.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Shev83
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import urllib,urllib2,re,xbmcplugin,xbmcgui,xbmcvfs,xbmc,xbmcaddon,HTMLParser,json,os,time,datetime,binascii
 
 h = HTMLParser.HTMLParser()
@@ -11,7 +28,7 @@ fanart = os.path.join(addonfolder,'fanart.jpg')
 down_path = selfAddon.getSetting('download-folder') 
 	
 def CATEGORIES():
-	addDir('FILMES','http://www.redcouch.me/filmes/',1,'http://digitalsherpa.com/wp-content/uploads/2013/04/social-media-marketing-tool.png')
+	addDir('FILMES','http://www.redcouch.me/filmes/',1,artfolder + 'categorias.png')
 
 def listar_videos(url):
     codigo_fonte = abrir_url(url)
